81f36d99555622090a471b1e2ecfedf2b9c204f7
Saif Ur Rehman <saif@paralogic.io>
2024-05-18 07:58:38 +0500

    feat: Fabric Ledger (Draft)

---
+++ textile/textile/report/fabric_ledger/fabric_ledger.js
@@ -0,0 +1,130 @@
+// Copyright (c) 2024, ParaLogic and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Fabric Ledger"] = {
+	"filters": [
+		{
+			"fieldname": "company",
+			"label": __("Company"),
+			"fieldtype": "Link",
+			"options": "Company",
+			"default": frappe.defaults.get_user_default("Company"),
+			"bold": 1
+		},
+		{
+			"fieldname": "from_date",
+			"label": __("From Date"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.add_months(frappe.datetime.get_today(), -1),
+			"reqd": 1
+		},
+		{
+			"fieldname": "to_date",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.get_today(),
+			"reqd": 1
+		},
+		{
+			"fieldname": "customer",
+			"label": __("Customer"),
+			"fieldtype": "Link",
+			"options": "Customer"
+		},
+		{
+			"fieldname": "item_code",
+			"label": __("Fabric Item"),
+			"fieldtype": "Link",
+			"options": "Item",
+			"get_query": function() {
+				let out = {
+					query: "erpnext.controllers.queries.item_query",
+					filters: {
+						"include_disabled": 1,
+						"textile_item_type": ["in", ["Greige Fabric", "Ready Fabric"]]
+					}
+				}
+
+				let customer = frappe.query_report.get_filter_value("customer");
+				if (customer) {
+					out.filters['customer'] = customer;
+				}
+
+				return out;
+			},
+			on_change: function() {
+				let item_code = frappe.query_report.get_filter_value('item_code');
+				if (!item_code) {
+					frappe.query_report.set_filter_value('item_name', "");
+				} else {
+					frappe.db.get_value("Item", item_code, 'item_name', function(value) {
+						frappe.query_report.set_filter_value('item_name', value['item_name']);
+					});
+				}
+			}
+		},
+		{
+			"fieldname": "item_name",
+			"label": __("Fabric Name"),
+			"fieldtype": "Data",
+			"read_only": 1,
+		},
+		{
+			"fieldname": "batch_no",
+			"label": __("Batch No"),
+			"fieldtype": "Link",
+			"options": "Batch",
+			"get_query": function() {
+				let filters = {};
+
+				let item_code = frappe.query_report.get_filter_value("item_code");
+				if (item_code) {
+					filters['item'] = item_code;
+				}
+
+				return {
+					filters: filters
+				};
+			},
+		},
+		{
+			"fieldname": "hide_internal_entries",
+			"label": __("Hide Internal Entries"),
+			"fieldtype": "Check",
+			"default": 1,
+		},
+		{
+			"fieldname": "merge_print_production_entries",
+			"label": __("Merge Print Production Entries"),
+			"fieldtype": "Check",
+			"default": 1,
+		},
+		{
+			"fieldname": "combine_ready_greige_fabric",
+			"label": __("Combine Ready and Greige Fabric"),
+			"fieldtype": "Check",
+			"default": 1,
+		},
+	],
+	formatter: function(value, row, column, data, default_formatter) {
+		var style = {};
+
+		$.each(['in_qty', 'out_qty'], function (i, f) {
+			if (flt(value)) {
+				if (column.fieldname === 'in_qty') {
+					style['color'] = 'green';
+				}
+				if (column.fieldname === 'out_qty') {
+					style['color'] = 'red';
+				}
+
+				if (data.is_internal_entry && ['in_qty', 'out_qty'].includes(column.fieldname)) {
+					style['color'] = 'var(--gray-600)';
+				}
+			}
+		});
+
+		return default_formatter(value, row, column, data, {css: style});
+	},
+};
---
+++ textile/textile/report/fabric_ledger/fabric_ledger.py
@@ -0,0 +1,288 @@
+# Copyright (c) 2013, Frappe Technologies Pvt. Ltd. and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe import _
+from frappe.utils import flt
+
+
+def execute(filters=None):
+	return FabricLedger(filters).run()
+
+
+class FabricLedger:
+	def __init__(self, filters=None):
+		self.filters = frappe._dict(filters or {})
+		self.show_item_name = frappe.defaults.get_global_default('item_naming_by') != "Item Name"
+		self.show_customer_name = frappe.defaults.get_global_default('cust_master_name') == "Naming Series"
+		self.has_batches = False
+
+	def run(self):
+		self.validate_filters()
+		self.get_items()
+		self.get_data()
+		self.prepare_rows()
+		self.get_columns()
+
+		return self.columns, self.rows
+
+	def validate_filters(self):
+		if self.filters.item_code:
+			textile_item_type = frappe.db.get_value("Item", self.filters.item_code, "textile_item_type")
+			if textile_item_type not in ("Greige Fabric", "Ready Fabric"):
+				frappe.throw("Item must be Greige Fabric or Ready Fabric")
+
+		self.filters.rejected_warehouses = [
+			frappe.db.get_single_value("Fabric Printing Settings", "default_printing_rejected_warehouse"),
+			frappe.db.get_single_value("Fabric Pretreatment Settings", "default_pretreatment_rejected_warehouse"),
+		]
+		self.filters.rejected_warehouses = [v for v in self.filters.rejected_warehouses if v]
+
+	def get_items(self):
+		self.ready_fabric_items = []
+		self.greige_fabric_items = []
+		self.printed_fabric_items = []
+
+		if self.filters.item_code:
+			textile_item_type = frappe.db.get_value("Item", self.filters.item_code, "textile_item_type")
+			if textile_item_type == "Greige Fabric":
+				self.greige_fabric_items = [self.filters.item_code]
+
+				if self.filters.combine_ready_greige_fabric:
+					self.ready_fabric_items = frappe.get_all("Item", filters={
+						"fabric_item": self.filters.item_code, "textile_item_type": "Ready Fabric"
+					}, pluck="name")
+
+			elif textile_item_type == "Ready Fabric":
+				self.ready_fabric_items = [self.filters.item_code]
+
+				if self.filters.combine_ready_greige_fabric:
+					greige_fabric_item = frappe.db.get_value("Item", self.filters.item_code, "fabric_item")
+					if greige_fabric_item:
+						self.greige_fabric_items = [greige_fabric_item]
+
+			if self.ready_fabric_items:
+				self.printed_fabric_items = frappe.get_all("Item", filters={
+					"textile_item_type": "Printed Design", "fabric_item": ("in", self.ready_fabric_items)
+				}, pluck="name")
+
+		elif self.filters.customer:
+			self.ready_fabric_items = frappe.get_all("Item", filters={
+				"customer": self.filters.customer, "textile_item_type": "Ready Fabric"
+			}, pluck="name")
+			self.greige_fabric_items = frappe.get_all("Item", filters={
+				"customer": self.filters.customer, "textile_item_type": "Greige Fabric"
+			}, pluck="name")
+			self.printed_fabric_items = frappe.get_all("Item", filters={
+				"customer": self.filters.customer, "textile_item_type": "Printed Design"
+			}, pluck="name")
+
+		self.filters.item_codes = list(set(self.greige_fabric_items + self.ready_fabric_items + self.printed_fabric_items))
+
+	def get_data(self):
+		self.data = []
+		self.opening_qty = None
+
+		if not self.filters.item_codes:
+			return
+
+		conditions = self.get_conditions()
+
+		if self.filters.from_date:
+			self.opening_qty = frappe.db.sql(f"""
+				select sum(sle.actual_qty)
+				from `tabStock Ledger Entry` sle
+				where sle.item_code in %(item_codes)s and sle.posting_date < %(from_date)s
+			""", self.filters)
+			self.opening_qty = flt(self.opening_qty[0][0]) if self.opening_qty else 0
+
+		self.data = frappe.db.sql(f"""
+			select sle.posting_date,
+				sle.voucher_type, sle.voucher_no,
+				sle.item_code, item.item_name,
+				sle.warehouse, sle.batch_no,
+				sle.party_type, sle.party,
+				item.textile_item_type,
+				fabric_item.name as fabric_item, fabric_item.item_name as fabric_item_name,
+				sle.actual_qty, sle.stock_uom as uom,
+				ste.work_order, ste.coating_order,
+				ste.print_order as ste_print_order, ste.pretreatment_order as ste_pretreatment_order,
+				dni.print_order as dni_print_order, dni.pretreatment_order as dni_pretreatment_order,
+				psi.print_order as psi_print_order, psi.pretreatment_order as psi_pretreatment_order
+			from `tabStock Ledger Entry` sle
+			inner join `tabItem` item on sle.item_code = item.name
+			left join `tabItem` fabric_item on fabric_item.name = item.fabric_item
+			left join `tabStock Entry` ste on ste.name = sle.voucher_no and sle.voucher_type = 'Stock Entry'
+			left join `tabDelivery Note Item` dni on dni.name = sle.voucher_detail_no and sle.voucher_type = 'Delivery Note'
+			left join `tabPacking Slip Item` psi on psi.name = sle.voucher_detail_no and sle.voucher_type = 'Packing Slip'
+			where sle.item_code in %(item_codes)s
+				{conditions}
+			order by sle.posting_date, sle.posting_time, sle.creation
+		""", self.filters, as_dict=1)
+
+	def get_conditions(self):
+		conditions = []
+
+		if self.filters.company:
+			conditions.append("sle.company = %(company)s")
+
+		if self.filters.from_date:
+			conditions.append("sle.posting_date >= %(from_date)s")
+
+		if self.filters.to_date:
+			conditions.append("sle.posting_date <= %(to_date)s")
+
+		if self.filters.batch_no:
+			conditions.append("sle.batch_no = %(batch_no)s")
+
+		return "AND {}".format(" AND ".join(conditions)) if conditions else ""
+
+	def prepare_rows(self):
+		self.rows = []
+		if not self.data:
+			return
+
+		# Preprocess Data
+		for sle in self.data:
+			sle["disable_item_formatter"] = 1
+
+			if sle.textile_item_type != "Printed Design":
+				sle.fabric_item = sle.item_code
+				sle.fabric_item_name = sle.item_name
+
+			sle.document_type = sle.voucher_type
+			sle.document_no = sle.voucher_no
+			if sle.ste_print_order and self.filters.merge_print_production_entries:
+				sle.document_type = "Print Order"
+				sle.document_no = sle.ste_print_order
+
+			sle.print_order = sle.ste_print_order or sle.dni_print_order or sle.psi_print_order
+			sle.pretreatment_order = sle.ste_pretreatment_order or sle.dni_pretreatment_order or sle.psi_pretreatment_order
+
+			if sle.print_order:
+				sle.order_type = "Print Order"
+				sle.order_no = sle.print_order
+			elif sle.pretreatment_order:
+				sle.order_type = "Pretreatment Order"
+				sle.order_no = sle.pretreatment_order
+			elif sle.coating_order:
+				sle.order_type = "Coating Order"
+				sle.order_no = sle.coating_order
+
+			sle.is_rejection = 1 if sle.warehouse in self.filters.rejected_warehouses else 0
+
+		# Group rows by voucher and fabric item
+		voucher_map = {}
+		for sle in self.data:
+			voucher_key = (sle.posting_date, sle.document_type, sle.document_no)
+			voucher_dict = voucher_map.setdefault(voucher_key, frappe._dict({"actual_qty": 0, "fabric_items": {}}))
+
+			row = voucher_dict.fabric_items.get(sle.fabric_item)
+			if not row:
+				row = voucher_map[voucher_key].fabric_items[sle.fabric_item] = sle.copy()
+				row.actual_qty = 0
+				row.in_qty = 0
+				row.out_qty = 0
+				row.rejected_qty = 0
+
+			voucher_dict.actual_qty += sle.actual_qty
+			row.actual_qty += sle.actual_qty
+			row.qty_after_transaction = row.actual_qty
+
+			if sle.is_rejection:
+				row.rejected_qty += sle.actual_qty
+
+			if flt(sle.actual_qty) >= 0:
+				row.in_qty += sle.actual_qty
+			else:
+				row.out_qty += -sle.actual_qty
+
+		# Opening Row
+		if self.opening_qty is not None:
+			self.rows.append(self.get_opening_row())
+
+		# Movement Rows
+		for voucher_dict in voucher_map.values():
+			for row in voucher_dict.fabric_items.values():
+				row.is_internal_entry = not flt(voucher_dict.actual_qty, 6) or not flt(row.actual_qty, 6)
+
+				if row.rejected_qty:
+					row.in_qty -= row.rejected_qty
+					row.out_qty -= row.rejected_qty
+
+				if not row.is_internal_entry or not self.filters.hide_internal_entries:
+					self.rows.append(row)
+
+				if row.rejected_qty:
+					rejected_row = row.copy()
+					rejected_row.in_qty = 0
+					rejected_row.out_qty = rejected_row.rejected_qty
+					rejected_row.actual_qty = -rejected_row.rejected_qty
+					rejected_row.qty_after_transaction = rejected_row.actual_qty
+					rejected_row.is_internal_entry = False
+					self.rows.append(rejected_row)
+
+		# Running balance qty and totals
+		accumulated_balance_qty = 0
+		total_in_qty = 0
+		total_out_qty = 0
+		for row in self.rows:
+			accumulated_balance_qty += row.qty_after_transaction
+			row.qty_after_transaction = accumulated_balance_qty
+
+			if not row.is_internal_entry:
+				total_in_qty += flt(row.in_qty)
+				total_out_qty += flt(row.out_qty)
+
+			if row.batch_no:
+				self.has_batches = True
+
+		# Closing Row
+		if self.opening_qty is not None:
+			self.rows.append(self.get_closing_row(accumulated_balance_qty, total_in_qty, total_out_qty))
+
+	def get_opening_row(self):
+		return frappe._dict({
+			"posting_date": self.filters.from_date,
+			"document_type": "Opening Stock",
+			"fabric_item": self.filters.item_code,
+			"fabric_item_name": frappe.db.get_value("Item", self.filters.item_code, "item_name", cache=1),
+			"uom": frappe.db.get_value("Item", self.filters.item_code, "stock_uom", cache=1),
+			"qty_after_transaction": self.opening_qty or 0,
+			"_bold": True,
+		})
+
+	def get_closing_row(self, closing_qty, in_qty, out_qty):
+		return frappe._dict({
+			"posting_date": self.filters.to_date,
+			"document_type": "Closing Stock",
+			"fabric_item": self.filters.item_code,
+			"fabric_item_name": frappe.db.get_value("Item", self.filters.item_code, "item_name", cache=1),
+			"uom": frappe.db.get_value("Item", self.filters.item_code, "stock_uom", cache=1),
+			"in_qty": in_qty,
+			"out_qty": out_qty,
+			"qty_after_transaction": closing_qty,
+			"_bold": True,
+		})
+
+	def get_columns(self):
+		columns = [
+			{"label": _("Date"), "fieldname": "posting_date", "fieldtype": "Date", "width": 95},
+			{"label": _("Voucher Type"), "fieldname": "document_type", "width": 120},
+			{"label": _("Voucher #"), "fieldname": "document_no", "fieldtype": "Dynamic Link", "options": "document_type", "width": 120},
+			{"label": _("Fabric Item"), "fieldname": "fabric_item", "fieldtype": "Link", "options": "Item", "width": 100},
+			{"label": _("Fabric Name"), "fieldname": "fabric_item_name", "fieldtype": "Data", "width": 230},
+			{"label": _("UOM"), "fieldname": "uom", "fieldtype": "Link", "options": "UOM", "width": 60},
+			{"label": _("In Qty"), "fieldname": "in_qty", "fieldtype": "Float", "width": 80},
+			{"label": _("Out Qty"), "fieldname": "out_qty", "fieldtype": "Float", "width": 80},
+			{"label": _("Balance Qty"), "fieldname": "qty_after_transaction", "fieldtype": "Float", "width": 100},
+			{"label": _("Party"), "fieldname": "party", "fieldtype": "Dynamic Link", "options": "party_type", "width": 150},
+			{"label": _("Order Type"), "fieldname": "order_type", "fieldtype": "Data", "width": 120},
+			{"label": _("Order #"), "fieldname": "order_no", "fieldtype": "Dynamic Link", "options": "order_type", "width": 120},
+			{"label": _("Batch No"), "fieldname": "batch_no", "fieldtype": "Link", "options": "Batch", "width": 150},
+		]
+
+		if not self.has_batches:
+			columns = [c for c in columns if c.get("fieldname") != "batch_no"]
+
+		self.columns = columns
